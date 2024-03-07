419da895c1a332622803276a75f9703a440c9325
Moshe Simantov <me@moshe.io>
2024-03-07 09:53:38 +0200

    Update GraphQL-See-Mercurius example with Vercel deployment

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # graphql-see-mercurius
 
-Example showing how to use graphql-sse with Mercurius.
+Example showing how to use graphql-sse with Mercurius on Vercel.
 
 It builds on top of https://www.the-guild.dev/blog/graphql-over-sse#with-fastify and adds mercurius to the picture, with minor adaptations.
 
@@ -24,3 +24,4 @@ This will check that queries and subscriptions work over plain HTTP (mercurius) 
 - Exposes the same schema both via mercurius and graphql-see
 - Mercurius listens on path `/graphql` whereas graphql-see on `/graphql-stream`
 - Any GraphQL operations can go to either one or the other
+- [Fastify serverless configuration](https://fastify.dev/docs/v4.15.x/Guides/Serverless/#vercel) is used to deploy to Vercel
--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-import Fastify from 'fastify' 
-import { createHandler } from 'graphql-sse'
-import mercurius from 'mercurius'
+import Fastify from "fastify";
+import { createHandler } from "graphql-sse";
+import mercurius from "mercurius";
 
-import { schema } from './schema.js'
- 
+import { schema } from "./schema.js";
+
 // Create the GraphQL over SSE handler
-const handler = createHandler({ schema })
- 
-const fastify = Fastify()
+const handler = createHandler({ schema });
+
+const fastify = Fastify();
 
 // Expose schema over plain mercurius
 fastify.register(mercurius, {
   schema,
-  graphiql: true
-})
+  graphiql: true,
+});
 
 // Delegate all requests to this path to graphql-sse
-fastify.all('/graphql/stream', (req, res) =>
+fastify.all("/graphql/stream", (req, res) =>
   handler(
     req.raw,
     res.raw,
     req.body // fastify reads the body for you
   )
-)
- 
-await fastify.listen({ port: 4000 })
-console.log('Listening to port 4000')
+);
+
+// await fastify.listen({ port: 4000 })
+// console.log('Listening to port 4000')
+
+export default async (req, res) => {
+  await fastify.ready();
+  fastify.server.emit("request", req, res);
+};
